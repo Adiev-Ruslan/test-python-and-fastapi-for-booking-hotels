d0264ef50ab8bcec26013ca66d09bb97b395bd24
Ruslan Adiev <ruslan.adieff@yandex.ru>
2024-12-21 21:30:45 +0300

    Добавлены .gitignore, requirements.txt и config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+	DB_NAME: str
+	
+	model_config = SettingsConfigDict(env_file=f"{Path(__file__).parent.parent/'.env'}")
+	
+	
+settings = Settings()
