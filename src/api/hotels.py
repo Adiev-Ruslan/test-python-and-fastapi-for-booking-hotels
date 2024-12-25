bff1e2f0daf5c44ae453af1efa58fe0cbcf8a192
Ruslan Adiev <ruslan.adieff@yandex.ru>
2024-12-25 20:46:19 +0300

    возможность отладки в методе create_hotel в api\hotels.py

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,4 +1,8 @@
 from fastapi import Query, APIRouter, Body
+from sqlalchemy import insert
+
+from src.database import async_session_maker, engine
+from src.models.hotels import HotelsOrm
 from src.schemas.hotels import Hotel, HotelPATCH
 from src.api.dependencies import PaginationDep
 
@@ -48,44 +52,19 @@ def get_hotels(
 	}
 
 
-"""
-@router.get(
-	"",
-	description="Получить в json-формате список всех отелей (либо некоторые из них)"
-)
-def get_hotels(
-	pagination: PaginationDep,
-	id: int | None = Query(None, description="Айдишник"),
-	title: str | None = Query(None, description="Название отеля"),
-):
-	
-	hotels_ = []
-	for hotel in hotels_:
-		if id and hotel["id"] != id:
-			continue
-		if title and hotel["title"] != title:
-			continue
-		hotels_.append(hotel)
-
-	if pagination.page and pagination.per_page:
-		return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
-	return hotels_
-"""
-
-
 @router.post("", description="Добавить в БД новый отель")
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
 	"1": {"summary": "Сочи", "value": {
 		"title": "Отель Сочи 5 звезд у моря",
-		"name": "sochi-u-morya"
+		"location": "ул. Моря 1"
 	}}})
 ):
-	global hotels
-	hotels.append({
-		"id": hotels[-1]["id"] + 1,
-		"title": hotel_data.title,
-		"name": hotel_data.name,
-	})
+	async with async_session_maker() as session:
+		add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+		print(add_hotel_stmt.compile(engine, compile_kwargs={"literal_binds": True}))
+		await session.execute(add_hotel_stmt)
+		await session.commit()
+	
 	return {"status": "OK"}
 
 
