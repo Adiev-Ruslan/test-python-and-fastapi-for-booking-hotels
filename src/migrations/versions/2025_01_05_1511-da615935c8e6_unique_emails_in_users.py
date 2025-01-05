f53182410262e31e738ef072df59a9ca9613275c
Ruslan Adiev <ruslan.adieff@yandex.ru>
2025-01-05 15:42:30 +0300

    теперь при аутентификация нового пользователя бд принимает только уникальный email

---
+++ src/migrations/versions/2025_01_05_1511-da615935c8e6_unique_emails_in_users.py
@@ -0,0 +1,28 @@
+"""unique emails in users
+
+Revision ID: da615935c8e6
+Revises: 5de53afab99e
+Create Date: 2025-01-05 15:11:10.373489
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "da615935c8e6"
+down_revision: Union[str, None] = "5de53afab99e"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(None, "users", ["email"])
+    
+
+def downgrade() -> None:
+    op.drop_constraint(None, "users", type_="unique")
+    
