71f096686ef414e49ede454bf8d16cb7b3e1ce3b
Ruslan Adiev <ruslan.adieff@yandex.ru>
2025-01-20 22:14:46 +0300

    Создал (не без помощи Артема) один общий менеджер контекста для всех ручек в api/auth.py и api/hotels.py.

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response, Request
 
-from src.api.dependencies import UserIdDep
-from src.repos.users import UsersRepository
-from src.database import async_session_maker
+from src.api.dependencies import UserIdDep, DBDep
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.services.auth import AuthService
 
@@ -10,27 +8,25 @@ router = APIRouter(prefix="/auth", tags=["Авторизация и аутент
 
 
 @router.post("/register", description="Ручка для аутентификации пользователя")
-async def register_user(data: UserRequestAdd):
+async def register_user(data: UserRequestAdd, db: DBDep):
 	hashed_password = AuthService().hash_password(data.password)
 	new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-	async with async_session_maker() as session:
-		await UsersRepository(session).add(new_user_data)
-		await session.commit()
+	await db.users.add(new_user_data)
+	await db.commit()
 		
 	return {"status": "OK"}
 
 
 @router.post("/login", description="Ручка для входа пользователя")
-async def login_user(data: UserRequestAdd, response: Response):
-	async with async_session_maker() as session:
-		user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
-		if not user:
-			raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
-		if not AuthService().verify_password(data.password, user.hashed_password):
-			raise HTTPException(status_code=401, detail="Пароль неверный")
-		access_token = AuthService().create_access_token({"user_id": user.id})
-		response.set_cookie("access_token", access_token)
-		return {"access_token": access_token}
+async def login_user(data: UserRequestAdd, response: Response, db: DBDep):
+	user = await db.users.get_user_with_hashed_password(email=data.email)
+	if not user:
+		raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+	if not AuthService().verify_password(data.password, user.hashed_password):
+		raise HTTPException(status_code=401, detail="Пароль неверный")
+	access_token = AuthService().create_access_token({"user_id": user.id})
+	response.set_cookie("access_token", access_token)
+	return {"access_token": access_token}
 	
 	
 @router.delete("/logout")
@@ -52,14 +48,13 @@ async def logout_user(request: Request, response: Response):
 	
 
 @router.get("/me")
-async def get_me(user_id: UserIdDep):
+async def get_me(user_id: UserIdDep, db: DBDep):
 	"""
 	Получает access_token из cookies пользователя.
 	Если токен отсутствует, возвращает сообщение об ошибке.
 	"""
 	
-	async with async_session_maker() as session:
-		user = await UsersRepository(session).get_one_or_none(id=user_id)
-		return user
+	user = await db.users.get_one_or_none(id=user_id)
+	return user
 	
 	
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,9 +1,7 @@
-from fastapi import Query, APIRouter, Body, HTTPException
+from fastapi import Query, APIRouter, Body
 
-from src.repos.hotels import HotelsRepository
-from src.database import async_session_maker
 from src.schemas.hotels import HotelPATCH, HotelAdd
-from src.api.dependencies import PaginationDep
+from src.api.dependencies import PaginationDep, DBDep
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
@@ -11,78 +9,67 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 @router.get("")
 async def get_hotels(
 	pagination: PaginationDep,
+	db: DBDep,
 	title: str | None = Query(None, description="Часть названия отеля"),
 	location: str | None = Query(None, description="Часть названия местоположения отеля")
 ):
 	"""Получить в json-формате список всех отелей"""
 	
 	per_page = pagination.per_page or 5
-	async with async_session_maker() as session:
-		return await HotelsRepository(session).get_all(
-			location=location,
-			title=title,
-			limit=per_page,
-			offset=per_page * (pagination.page - 1)
-		)
-	
+	return await db.hotels.get_all(
+		location=location,
+		title=title,
+		limit=per_page,
+		offset=per_page * (pagination.page - 1)
+	)
+
 
 @router.get("/{hotel_id}")
-async def get_hotel(hotel_id: int):
+async def get_hotel(hotel_id: int, db: DBDep):
 	"""Ручка для получения по id только одного конкретного отеля"""
 	
-	async with async_session_maker() as session:
-		hotel = await HotelsRepository(session).get_one_or_none(id=hotel_id)
-		if hotel is None:
-			raise HTTPException(status_code=404, detail="Нет такого отеля")
-		return {"status": "OK", "data": hotel}
-
-
+	return await db.hotels.get_one_or_none(id=hotel_id)
+	
+	
 @router.post("")
-async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples={
-	"1": {"summary": "Сочи", "value": {
-		"title": "Отель Сочи 5 звезд у моря",
-		"location": "ул. Моря 1"
+async def create_hotel(
+	db: DBDep,
+	hotel_data: HotelAdd = Body(openapi_examples={
+		"1": {"summary": "Сочи", "value": {
+			"title": "Отель Сочи 5 звезд у моря",
+			"location": "ул. Моря 1"
 	}}})
 ):
 	"""Добавить в БД новый отель"""
 	
-	async with async_session_maker() as session:
-		hotel = await HotelsRepository(session).add(hotel_data)
-		await session.commit()
-	
+	hotel = await db.hotels.add(hotel_data)
+	await db.commit()
 	return {"status": "OK", "data": hotel}
 
 
 @router.put("/{hotel_id}")
-async def change_all_data_in_hotel(hotel_id: int, hotel_data: HotelAdd):
+async def change_all_data_in_hotel(hotel_id: int, hotel_data: HotelAdd, db: DBDep):
 	"""Изменить все данные отеля"""
 	
-	async with async_session_maker() as session:
-		await HotelsRepository(session).edit(hotel_data, id=hotel_id)
-		await session.commit()
+	await db.hotels.edit(hotel_data, id=hotel_id)
+	await db.commit()
 	return {"status": "OK"}
 
 
 @router.patch("/{hotel_id}")
-async def change_some_data_in_hotel(hotel_id: int, hotel_data: HotelPATCH):
+async def change_some_data_in_hotel(hotel_id: int, hotel_data: HotelPATCH, db: DBDep):
 	"""Изменить некоторые данные отеля"""
 	
-	async with async_session_maker() as session:
-		await HotelsRepository(session).edit(
-			hotel_data,
-			exclude_unset=True,
-			id=hotel_id
-		)
-		await session.commit()
+	await db.hotels.edit(hotel_data, exclude_unset=True, id=hotel_id)
+	await db.commit()
 	return {"status": "OK"}
 	
 	
 @router.delete("/{hotel_id}")
-async def delete_hotel(hotel_id: int):
+async def delete_hotel(hotel_id: int, db: DBDep):
 	"""Удалить отель из БД по его id"""
 	
-	async with async_session_maker() as session:
-		await HotelsRepository(session).delete(id=hotel_id)
-		await session.commit()
+	await db.hotels.delete(id=hotel_id)
+	await db.commit()
 	return {"status": "OK"}
 	
