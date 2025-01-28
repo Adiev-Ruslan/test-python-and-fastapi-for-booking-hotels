2d93ed227cc217b476282f8df4023d3d28ea4ef4
Ruslan Adiev <ruslan.adieff@yandex.ru>
2025-01-28 12:49:04 +0300

    Написал ручки get_all_bookings и get_authorized_bookings.

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,9 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from src.models.users import UsersOrm
+from src.api.dependencies import get_current_user_id
 from src.api.dependencies import DBDep, UserIdDep
 from src.schemas.bookings import BookingAddRequest, BookingAdd
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
+
+
+@router.get("")
+async def get_all_bookings(db: DBDep):
+	"""Получить все бронирования"""
+	bookings = await db.bookings.get_all()
+	return {"status": "OK", "bookings": bookings}
+
+
+@router.get("/me")
+async def get_authorized_bookings(
+	db: DBDep,
+	current_user: UsersOrm = Depends(get_current_user_id)
+):
+	"""Получить бронирования авторизованного пользователя"""
+	bookings = await db.bookings.get_by_user_id(user_id=current_user.id)
+	return bookings
 
 
 @router.post("")
@@ -19,7 +38,7 @@ async def create_booking(
 	_booking_data = BookingAdd(
 		user_id=user_id,
 		price=room_price,
-		**booking_data.dict()
+		**booking_data.model_dump()
 	)
 	booking = await db.bookings.add(_booking_data)
 	await db.commit()
--- src/repos/bookings.py
+++ src/repos/bookings.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from src.models.bookings import BookingsOrm
 from src.repos.base import BaseRepository
 from src.schemas.bookings import Booking
@@ -6,4 +8,14 @@ from src.schemas.bookings import Booking
 class BookingsRepository(BaseRepository):
 	model = BookingsOrm
 	schema = Booking
+	
+	async def get_all(self):
+		query = select(self.model)
+		result = await self.session.execute(query)
+		return result.scalars().all()
+	
+	async def get_by_user_id(self, user_id: int):
+		query = select(self.model).where(self.model.user_id == user_id)
+		result = await self.session.execute(query)
+		return result.scalars.all()
 	
