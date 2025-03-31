3dcfd3e34344ec84adc4a7b82fe950490e6f43dd
Ruslan Adiev <ruslan.adieff@yandex.ru>
2025-03-31 18:38:22 +0300

    Коммит для задания 19.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
+import json
 import os
 import pytest
 import asyncio
 from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession
 
 # Загружаем тестовый .env перед импортом config
 os.environ["MODE"] = "TEST"
@@ -9,7 +11,7 @@ load_dotenv(".env-test")
 
 from httpx import AsyncClient, ASGITransport
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker
 from src.models import *
 from src.main import app
 
@@ -35,6 +37,28 @@ async def setup_database(check_test_mode):
 	async with engine_null_pool.begin() as conn:
 		await conn.run_sync(Base.metadata.drop_all)
 		await conn.run_sync(Base.metadata.create_all)
+
+	async with async_session_maker() as session:
+		await insert_mock_data(session)
+
+
+async def insert_mock_data(session: AsyncSession):
+	"""Заполняет тестовую БД мок-данными"""
+	
+	with open("tests/mock_hotels.json", encoding="utf-8") as f:
+		hotels_data = json.load(f)
+		
+	with open("tests/mock_rooms.json", encoding="utf-8") as f:
+		rooms_data = json.load(f)
+		
+	hotels = [HotelsOrm(**hotel) for hotel in hotels_data]
+	session.add_all(hotels)
+	await session.flush()
+	
+	rooms = [RoomsOrm(**room) for room in rooms_data]
+	session.add_all(rooms)
+	
+	await session.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
