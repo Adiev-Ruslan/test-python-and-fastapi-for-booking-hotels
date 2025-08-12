8766e962cb5dd3fb95a2a49b41c27172c2f64bcd
Ruslan Adiev <ruslan.adieff@yandex.ru>
2025-08-12 17:23:03 +0300

    Запилил readme.md, где описаны docker-контейнеры.

--- src/main.py
+++ src/main.py
@@ -52,6 +52,6 @@ async def custom_swagger_ui_html():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True)
+    uvicorn.run("main:app", reload=True)
 
 # python main.py - запуск удаленного сервера
