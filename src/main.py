0de559e999534074185f8be7c7a5eb40ac3aaa80
Ruslan Adiev <ruslan.adieff@yandex.ru>
2025-08-20 13:05:04 +0300

    Продолжаю изучать docker и его команды.

--- src/main.py
+++ src/main.py
@@ -52,6 +52,11 @@ async def custom_swagger_ui_html():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
 
 # python main.py - запуск удаленного сервера
--- src/tasks/celery_app.py
+++ src/tasks/celery_app.py
@@ -1,7 +1,11 @@
 from celery import Celery
 from src.config import settings
 
-celery_instance = Celery("tasks", broker=settings.REDIS_URL, include=["src.tasks.tasks"])
+celery_instance = Celery(
+    "tasks",
+    broker=settings.REDIS_URL,
+    include=["src.tasks.tasks"]
+)
 
 celery_instance.conf.beat_schedule = {
     "gachi-muchi": {"task": "booking_today_checkin", "schedule": 5}
